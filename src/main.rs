478e9a9ee01bad3dd65c02fde410d4ceea821f27
Guilhermo Masid <guilhermomasid@gmail.com>
2022-04-18 19:28:46 -0300

    chore: delay route

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@
 extern crate rocket;
 
 use rocket::response::content;
+use rocket::tokio::time::{sleep, Duration};
 
 #[get("/test")]
 fn index() -> &'static str {
   "Hello Guilhermo"
+}
+
+#[get("/sleep/<seconds>")]
+async fn delay(seconds: u64) -> String {
+  sleep(Duration::from_secs(seconds)).await;
+  format!("Program sleeped {} seconds", seconds)
 }
 
 #[catch(404)]
@@ -16,6 +23,6 @@ fn not_found_route() -> content::Json<&'static str> {
 #[launch]
 fn rocket() -> _ {
   rocket::build()
-    .mount("/", routes![index])
+    .mount("/", routes![index, delay])
     .register("/", catchers![not_found_route])
 }
