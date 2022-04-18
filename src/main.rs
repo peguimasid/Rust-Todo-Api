a0bde3c0d9072ed2c06e234489217fd7f4f7bff4
Guilhermo Masid <guilhermomasid@gmail.com>
2022-04-18 19:21:36 -0300

    not found route

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
+use rocket::response::content;
+
+#[get("/test")]
 fn index() -> &'static str {
-    "Hello Guilhermo"
+  "Hello Guilhermo"
+}
+
+#[catch(404)]
+fn not_found_route() -> content::Json<&'static str> {
+  content::Json("{'error': 'route not found'}")
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+  rocket::build()
+    .mount("/", routes![index])
+    .register("/", catchers![not_found_route])
 }
