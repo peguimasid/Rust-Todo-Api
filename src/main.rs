628a5b0cdbec1fe9a1a0466eadec75884aa242a0
Guilhermo Masid <guilhermomasid@gmail.com>
2022-04-20 16:10:11 -0300

    add dependencies, install diesel_cli with cargo install run diesel setup to create migrations folder

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,13 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
 #[macro_use]
 extern crate rocket;
 
-use rocket::response::content;
-
-use rocket::fs::TempFile;
-
-#[post("/upload", format = "multipart", data = "<file>")]
-async fn upload(mut file: TempFile<'_>) -> std::io::Result<()> {
-  file.persist_to("./tmp").await
+#[get("/hello/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+  format!("Hello, {} year old named {}!", age, name)
 }
 
-#[get("/hello/<name>/<age>/<cool>", format = "application/json")]
-fn index(name: &str, age: u8, cool: bool) -> String {
-  if cool {
-    format!("You're a cool {} year old, {}!", age, name)
-  } else {
-    format!("{}, we need to talk about coolness.", name)
-  }
-}
-
-#[catch(404)]
-fn not_found_route() -> content::Json<&'static str> {
-  content::Json("{'error': 'route not found'}")
-}
-
-#[launch]
-fn rocket() -> _ {
-  rocket::build()
-    .mount("/", routes![index, upload])
-    .register("/", catchers![not_found_route])
+fn main() {
+  rocket::ignite().mount("/", routes![hello]).launch();
 }
