954265d5e6848768af726eaf601616723f329491
Guilhermo Masid <guilhermomasid@gmail.com>
2022-04-21 16:25:44 -0300

    chore: create model.rs file

---
+++ src/model.rs
@@ -0,0 +1,13 @@
+#[derive(Queryable, AsChangeset, Serialize, Deserialize, Debug)]
+#[table_name = "todos"]
+pub struct Todo {
+  pub id: i32,
+  pub title: String,
+  pub done: bool,
+}
+
+#[derive(Insertable, Serialize, Deserialize)]
+#[table_name = "todos"]
+pub struct NewTodo {
+  pub title: String,
+}
